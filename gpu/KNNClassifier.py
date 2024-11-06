b93093caf1febd5615af01295e10ed23d4834db1
Luca Venerando Greco <lucag2001@gmail.com>
2024-11-06 02:02:33 +0100

    feat: added multigpu implementation

---
+++ gpu/KNNClassifier.py
@@ -0,0 +1,42 @@
+from KNNClassifier import KNNClassifier
+import numpy as np
+from numba.cuda import pinned_array_like 
+import sys
+
+if len(sys.argv) < 4:
+    print('Usage: python test_knn.py <rows> <cols> <k>')
+    sys.exit(1)
+
+# Example with random data
+rows = int(sys.argv[1])
+cols = int(sys.argv[2])
+k = int(sys.argv[3])
+
+np.random.seed(699)
+X_train_pageable = np.random.rand(rows*cols).reshape((rows,cols))
+y_train_pageable = np.random.randint(2, size=rows)
+
+X_train = pinned_array_like(X_train_pageable)
+y_train = pinned_array_like(y_train_pageable)
+
+X_train[:] = X_train_pageable
+y_train[:] = y_train_pageable
+
+print(f'X_train shape {X_train.shape} - y_train shape {y_train.shape}')
+
+knn = KNNClassifier(k=k)
+knn.fit(X_train, y_train)
+
+# Create random indices to test
+test_size = 1000
+X_test_pageable = np.random.randint(rows, size=test_size)
+X_test = pinned_array_like(X_test_pageable)
+X_test[:] = X_test_pageable
+
+# Generate Predictions
+predictions = knn.predict(X_train[X_test])
+#print(f'Prediction {predictions}')
+#print(f'Label      {y_train[X_test]}')
+# Calculate the number of equal elements
+print(f'correct {np.sum(y_train[X_test] == predictions)}')
+
---
+++ gpu/test_knn.py
@@ -0,0 +1,73 @@
+import numpy as np
+from numba import cuda, guvectorize, jit, njit
+import math
+import cupy as cp
+from tqdm import tqdm
+
+# CUDA kernel to compute Euclidean distance
+@cuda.jit
+def euclidean_distance_kernel(x, data, distances):
+    i = cuda.grid(1)
+    if i < data.shape[0]:
+        diff = 0
+        for j in range(data.shape[1]):
+            diff += (x[j] - data[i][j])**2
+        distances[i] = math.sqrt(diff)
+
+class KNNClassifier:
+    def __init__(self, k, n_gpus = 4):
+        self.k = k
+        self.n_gpus = n_gpus
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+        self.d_X_train = []
+        self.d_y_train = []
+        self.streams = []
+        self.n_points_per_gpu = X.shape[0] // self.n_gpus
+        for i in range(self.n_gpus):
+            cuda.select_device(i)
+            self.streams.append(cuda.stream())
+            start = i*self.n_points_per_gpu
+            end = min((i+1)*self.n_points_per_gpu, X.shape[0])
+            self.d_X_train.append(cuda.to_device(X[start:end], stream=self.streams[i]))
+            self.d_y_train.append(cuda.to_device(y[start:end], stream=self.streams[i]))
+
+    def predict(self, X):
+        # y_pred = [self._predict(x) for x in X]
+        y_pred = []
+        for x in tqdm(X):
+            y_pred.append(self._predict(x))
+        return np.array(y_pred)
+    
+    def euclidean_distance(self, x, y):
+        return np.linalg.norm(x - y)
+
+    def _predict(self, x):
+        n_points = self.X_train.shape[0]
+
+        threads_per_block = (256)
+        blocks_per_grid = (n_points + (threads_per_block - 1)) // threads_per_block
+
+        d_distances = []
+        x_devices = []
+
+        for i in range(self.n_gpus):
+            cuda.select_device(i)
+            d_distances.append(cuda.device_array(self.d_X_train[i].shape[0], stream=self.streams[i]))
+            x_devices.append(cuda.to_device(x, stream=self.streams[i]))
+
+        for i in range(self.n_gpus):
+            cuda.select_device(i)
+            euclidean_distance_kernel[blocks_per_grid, threads_per_block, self.streams[i]](x_devices[i], self.d_X_train[i], d_distances[i])
+        
+        distances = [d.copy_to_host(stream=self.streams[i]) for i, d in enumerate(d_distances)]
+        distances = np.concatenate(distances)
+
+        k_nearest = cp.argsort(distances)[:self.k]
+
+        k_nearest_labels = self.y_train[k_nearest]
+
+        return np.bincount(k_nearest_labels).argmax()
+         
