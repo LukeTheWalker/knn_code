4e48924ba4177bd9a59ec9f6fb643b48806af382
elia <vagliettielia@gmail.com>
2024-11-06 18:48:01 +0100

    FEAT data generator using sbatch and bash

--- script/computational_tools/test_bench.sh
+++ script/computational_tools/test_bench.sh
@@ -2,9 +2,14 @@
 
 # Start doing for every cluster the baseline, if not already done
 # Create the folder results and go inside
+#################################################
 
-if "$1" == "y";
-then
+#ATTENTIOONNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+#FIRST DO THE FIRST, THEN THE SECOND
+
+
+
+if [ "$1" == "y" ]; then
     mkdir results_testbench
     cd results_testbench
     for i in 2000 4000 8000 10000 16000 32000 64000 100000 128000
@@ -16,12 +21,23 @@ then
     done
 fi
 
-if ["$2" == "y"];
-then
+
+if [ "$2" == "y" ]; then
+    cd results_testbench
+    touch data.csv
     csv_file="data.csv"
+    echo "dim,time" >> "$csv_file"
 
     for i in 2000 4000 8000 10000 16000 32000 64000 100000 128000
         do
-        echo "$i" >> "$csv_file" 
+        cp result_$i/baseline.txt baseline.txt
+        cat "baseline.txt" >> "$csv_file"
+        rm baseline.txt
     done
+
+    mv data.csv ../../../data_iris/baseline
+
+    cd ..
+
+    rm -r results_testbench
 fi
