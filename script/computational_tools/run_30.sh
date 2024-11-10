6227517d14b786765b9c0d7a2843ddcf9cffc69e
elia <vagliettielia@gmail.com>
2024-11-10 16:03:40 +0100

    FIX

--- script/computational_tools/run_30.sh
+++ script/computational_tools/run_30.sh
@@ -3,9 +3,7 @@
 # Start doing for every cluster the baseline, if not already done
 # Create the folder results and go inside
 #################################################
-echo prova
 cd script/computational_tools/results_testbench
-echo cacca
 for i in {1..30}
     do
     mkdir result_$i
