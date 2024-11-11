2dc8a4dd980602f403e9c9365fe6cafd42cdc00f
Bice Marzagora <59375165+BiceMar@users.noreply.github.com>
2024-11-11 17:26:46 +0100

    Update KNNClassifier.py

--- src/mpi_thread_pool/KNNClassifier.py
+++ src/mpi_thread_pool/KNNClassifier.py
@@ -1,21 +1,28 @@
 import numpy as np
-from mpi4py import MPI
+import os
 from concurrent.futures import ThreadPoolExecutor
 
 class KNNClassifier:
-    def __init__(self, k):
+    def __init__(self, k=3):
         self.k = k
 
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
 
+    def euclidean_distance2(self, x1, x2):
+        diff = (x1 - x2)
+        sqr_diff = diff ** 2
+        sqr_diff_sum = np.sum(sqr_diff)
+        return np.sqrt(sqr_diff_sum)
+
     def euclidean_distance(self, x1, X_train):
         return np.sqrt(((x1 - X_train) ** 2).sum(axis=1))
 
     def predict(self, X):
-        with ThreadPoolExecutor() as executor:
+        with ThreadPoolExecutor(max_workers=int(os.environ.get('SLURM_CPUS_PER_TASK', 16))) as executor:
             y_pred = list(executor.map(self._predict, X))
+
         return np.array(y_pred)
 
     def _predict(self, x):
